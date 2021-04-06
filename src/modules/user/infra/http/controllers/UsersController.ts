40077caac264fd4f1d86969f3501201f073778c7
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-06 20:07:54 -0300

    feat: add method create to users controller

--- src/modules/user/infra/http/controllers/UsersController.ts
+++ src/modules/user/infra/http/controllers/UsersController.ts
@@ -1,5 +1,22 @@
 import { Response, Request } from 'express';
+import { container } from 'tsyringe';
+import CreateUserService from '../../../services/CreateUserService';
 
-class UsersController {}
+class UsersController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { birth, name, password, cpf } = request.body;
+    const createUserService = container.resolve(CreateUserService);
+    const user = await createUserService.execute({
+      birth,
+      name,
+      password,
+      cpf,
+    });
+
+    const returnUser = { ...user, password: null };
+
+    return response.status(201).json(returnUser);
+  }
+}
 
 export default UsersController;
