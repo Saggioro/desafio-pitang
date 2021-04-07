86fc86a58e6e7ecadda58689603f6d4f68409edc
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-07 16:48:55 -0300

    feat: add create method in nurses controller

--- src/modules/nurse/infra/http/controllers/NursesController.ts
+++ src/modules/nurse/infra/http/controllers/NursesController.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import CreateNurseService from 'modules/nurse/services/CreateNurseService';
 
 class NursesController {
-  public async create(
-    request: Request,
-    response: Response,
-  ): Promise<Response> {}
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, password, email } = request.body;
+
+    const createNurseService = container.resolve(CreateNurseService);
+    const nurse = await createNurseService.execute({ name, password, email });
+
+    return response.status(201).json(nurse);
+  }
 }
 
 export default NursesController;
