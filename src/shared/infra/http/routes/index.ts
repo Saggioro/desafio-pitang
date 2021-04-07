69b6fa9a671aed793ab6b38663a10bb47196d162
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-07 17:04:58 -0300

    feat: add nurses router on global router

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,10 +2,13 @@ import { Router } from 'express';
 
 import usersRouter from '../../../../modules/user/infra/http/routes/users.routes';
 import sessionsUserRouter from '../../../../modules/user/infra/http/routes/sessionsUser.routes';
+import nursesRouter from '../../../../modules/nurse/infra/http/routes/nurses.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessionsUser', sessionsUserRouter);
+
+routes.use('/nurses', nursesRouter);
 
 export default routes;
