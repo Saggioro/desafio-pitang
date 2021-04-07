c111555520ecad676249f53437a932b0a388c633
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-07 16:36:03 -0300

    feat: add service to create nurses

---
+++ src/modules/nurse/services/CreateNurseService.ts
@@ -0,0 +1,40 @@
+import IHashProvider from 'shared/container/providers/hashProvider/models/IHashProvider';
+import { inject, injectable } from 'tsyringe';
+import ICreateNurseDTO from '../dtos/ICreateNurseDTO';
+import INursesRepository from '../repositories/INursesRepository';
+import AppError from '../../../shared/errors/AppError';
+import Nurse from '../infra/typeorm/entities/Nurse';
+
+@injectable()
+class CreateNurseService {
+  constructor(
+    @inject('NursesRepository')
+    private nursesRepository: INursesRepository,
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({
+    name,
+    email,
+    password,
+  }: ICreateNurseDTO): Promise<Nurse> {
+    const sameEmail = await this.nursesRepository.findByEmail(email);
+
+    if (sameEmail) {
+      throw new AppError('This email is already in use');
+    }
+
+    const hashedPassword = await this.hashProvider.generateHash(password);
+
+    const nurse = await this.nursesRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return nurse;
+  }
+}
+
+export default CreateNurseService;
