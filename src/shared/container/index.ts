9c82b819625a336bcd7bfaee337fe1b0b814969e
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-07 16:25:17 -0300

    feat: add nurse repository to tsyringe container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,8 +4,15 @@ import './providers';
 
 import UsersRepository from '../../modules/user/infra/typeorm/repositories/UsersRepository';
 import IUsersRepository from '../../modules/user/repositories/IUsersRepository';
+import INursesRepository from '../../modules/nurse/repositories/INursesRepository';
+import NursesRepository from '../../modules/nurse/infra/typeorm/repositories/NursesRepository';
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<INursesRepository>(
+  'NursesRepository',
+  NursesRepository,
 );
