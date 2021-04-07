fa6457e826e639576b9f84a0dcc3f8a72f395e02
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-07 16:05:55 -0300

    feat: add nurses controller

---
+++ src/modules/nurse/infra/http/controllers/NursesController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+
+class NursesController {
+  public async create(
+    request: Request,
+    response: Response,
+  ): Promise<Response> {}
+}
+
+export default NursesController;
