61371fadf3208ca23d3eacf0c2d324b028d3d321
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-07 16:51:15 -0300

    feat: add post route on nurses router

--- src/modules/nurse/infra/http/routes/nurses.routes.ts
+++ src/modules/nurse/infra/http/routes/nurses.routes.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import NursesController from '../controllers/NursesController';
 
 const nursesRouter = Router();
+
+const nursesController = new NursesController();
+
+nursesRouter.post('/', nursesController.create);
 
 export default nursesRouter;
