1cd82fc9cc934c6bba2cce62d504808b04bf2a66
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-08 15:17:56 -0300

    feat: add sessionsNurse route on global routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import usersRouter from '../../../../modules/user/infra/http/routes/users.routes';
 import sessionsUserRouter from '../../../../modules/user/infra/http/routes/sessionsUser.routes';
 import nursesRouter from '../../../../modules/nurse/infra/http/routes/nurses.routes';
+import sessionsNurseRouter from '../../../../modules/nurse/infra/http/routes/sessionsNurse.routes';
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.use('/users', usersRouter);
 routes.use('/sessionsUser', sessionsUserRouter);
 
 routes.use('/nurses', nursesRouter);
+routes.use('/sessionsNurse', sessionsNurseRouter);
 
 export default routes;
