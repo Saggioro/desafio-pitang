681c714f18288f146e4b4cfaa51ae109688db1bc
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-08 15:10:22 -0300

    feat: add sessions nurse controller with create method

---
+++ src/modules/nurse/infra/http/controllers/SessionsNurseController.ts
@@ -0,0 +1,17 @@
+import { container } from 'tsyringe';
+import { Request, Response } from 'express';
+import AuthenticateNurseService from 'modules/nurse/services/AuthenticateNurseService';
+
+class SessionsNurseController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+    const authenticateNurseService = container.resolve(
+      AuthenticateNurseService,
+    );
+
+    const token = await authenticateNurseService.execute({ email, password });
+
+    return response.json(token);
+  }
+}
+export default SessionsNurseController;
