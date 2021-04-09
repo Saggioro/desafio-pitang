f15e33168a3b2524ae03f40837f1613a777b85f9
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-09 17:26:16 -0300

    feat: add appointments controller

---
+++ src/modules/appointment/infra/http/controllers/AppointmentsController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+
+class AppointmentsController {
+  public async create(request: Request, response: Response) {}
+}
+
+export default AppointmentsController;
