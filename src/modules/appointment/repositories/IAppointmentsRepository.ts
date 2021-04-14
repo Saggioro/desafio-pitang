2716e54ca0a1041e15672ba03023efa0352ea850
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-13 21:06:16 -0300

    fix: remove find by user and find by nurse from appointments repository interface

--- src/modules/appointment/repositories/IAppointmentsRepository.ts
+++ src/modules/appointment/repositories/IAppointmentsRepository.ts
@@ -4,8 +4,6 @@ import Appointment from '../infra/typeorm/entities/Appointment';
 interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   save(appointment: Appointment): Promise<Appointment>;
-  findByUser(user_id: string): Promise<Appointment | undefined>;
-  findByNurse(nurse_id: string): Promise<Appointment[]>;
   findByDate(date: Date): Promise<Appointment | undefined>;
   findByDay(date: Date): Promise<Appointment[]>;
 }
