416327cfd08d700b3a99e120afd442a271e72e73
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-13 22:22:45 -0300

    feat: add appointment users repository

---
+++ src/modules/appointment/infra/typeorm/repositories/AppointmentUsersRepository.ts
@@ -0,0 +1,31 @@
+import ICreateAppointmentUserDTO from 'modules/appointment/dtos/ICreateAppointmentUserDTO';
+import IAppointmentUsersRepository from 'modules/appointment/repositories/IAppointmentUsersRepository';
+import { getRepository, Repository } from 'typeorm';
+import AppointmentUser from '../entities/AppointmentUser';
+
+class AppointmentUsersRepository implements IAppointmentUsersRepository {
+  private ormRepository: Repository<AppointmentUser>;
+
+  constructor() {
+    this.ormRepository = getRepository(AppointmentUser);
+  }
+
+  public async create(
+    data: ICreateAppointmentUserDTO,
+  ): Promise<AppointmentUser> {
+    const appointmentUser = this.ormRepository.create(data);
+
+    return this.ormRepository.save(appointmentUser);
+  }
+
+  public async save(
+    appointmentUser: AppointmentUser,
+  ): Promise<AppointmentUser> {
+    return this.ormRepository.save(appointmentUser);
+  }
+
+  public async findByUser(user_id: string): Promise<AppointmentUser[]> {
+    return this.ormRepository.find({ where: { user_id } });
+  }
+}
+export default AppointmentUsersRepository;
