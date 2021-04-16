6403cbeac9568a477e1c7dd39a6e7fcc4508b37f
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-16 02:05:28 -0300

    feat: add new secret to separate users and nurses authentications

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,10 @@
 export default {
   jwt: {
-    secret: process.env.APP_SECRET || 'ff3c1222b903113acbba54cc92536357',
+    userSecret:
+      process.env.APP_USER_SECRET || 'ff3c1222b903113acbba54cc92536357',
+    nurseSecret:
+      process.env.APP_NURSE_SECRET ||
+      'hr65165l416y5il4654f6ds54fa6s5d4g65j465iliu4tl6tyui8bv41',
     expiresIn: '1d',
   },
 };
