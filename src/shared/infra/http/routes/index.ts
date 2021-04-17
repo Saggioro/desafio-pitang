85a4dfbe2fb15eee0e672c38022ac27e2eb7025c
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-17 17:26:15 -0300

    feat: add appointments router to global routes file

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -4,6 +4,7 @@ import usersRouter from '../../../../modules/user/infra/http/routes/users.routes
 import sessionsUserRouter from '../../../../modules/user/infra/http/routes/sessionsUser.routes';
 import nursesRouter from '../../../../modules/nurse/infra/http/routes/nurses.routes';
 import sessionsNurseRouter from '../../../../modules/nurse/infra/http/routes/sessionsNurse.routes';
+import appointmentsRouter from '../../../../modules/appointment/infra/http/routes/appointments.routes';
 
 const routes = Router();
 
@@ -12,5 +13,7 @@ routes.use('/sessionsUser', sessionsUserRouter);
 
 routes.use('/nurses', nursesRouter);
 routes.use('/sessionsNurse', sessionsNurseRouter);
+
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
