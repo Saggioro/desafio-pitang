427fafbce0bf0d68979299d82e55e3c0393e4a0d
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-17 16:08:32 -0300

    feat: add post route for appointments

--- src/modules/appointment/infra/http/routes/appointments.routes.ts
+++ src/modules/appointment/infra/http/routes/appointments.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import ensureUserAuthenticated from '../../../../user/infra/http/middlewares/ensureUserAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
 
 const appointmentsController = new AppointmentsController();
 
-appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.post(
+  '/',
+  ensureUserAuthenticated,
+  appointmentsController.create,
+);
 
 export default appointmentsRouter;
