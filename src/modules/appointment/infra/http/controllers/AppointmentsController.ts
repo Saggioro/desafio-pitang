bb107e560d3c68e6d6921d9377a0c5b721f714e5
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-17 16:14:38 -0300

    refactor: add .env variables to create method on appointments controller

--- src/modules/appointment/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointment/infra/http/controllers/AppointmentsController.ts
@@ -4,15 +4,61 @@ import CreateAppointmentService from '../../../services/CreateAppointmentService
 
 class AppointmentsController {
   public async create(request: Request, response: Response): Promise<Response> {
-    const { id } = request.user;
+    const { id, birth } = request.user;
     const { date } = request.body;
+
+    let daysRange = 15;
+    let quantity = 2;
+    let duration = 30;
+    let dayQuantityLimit = 20;
+
+    if (
+      process.env.DAY_QUANTITY_LIMIT &&
+      !Number.isNaN(process.env.DAY_QUANTITY_LIMIT) &&
+      Number(process.env.DAY_QUANTITY_LIMIT) >= 1
+    ) {
+      dayQuantityLimit = Number(process.env.DAY_QUANTITY_LIMIT);
+    }
+    if (
+      (process.env.RANGE &&
+        !Number.isNaN(process.env.RANGE) &&
+        Number(process.env.RANGE) === 10) ||
+      Number(process.env.RANGE) === 15 ||
+      Number(process.env.RANGE) === 20
+    ) {
+      daysRange = Number(process.env.RANGE);
+    }
+
+    if (
+      (process.env.DURATION &&
+        !Number.isNaN(process.env.DURATION) &&
+        Number(process.env.DURATION) === 10) ||
+      Number(process.env.DURATION) === 15 ||
+      Number(process.env.DURATION) === 20 ||
+      Number(process.env.DURATION) === 30
+    ) {
+      duration = Number(process.env.DURATION);
+    }
+
+    if (
+      process.env.QUANTITY &&
+      !Number.isNaN(process.env.QUANTITY) &&
+      Number(process.env.QUANTITY) >= 1
+    ) {
+      quantity = Number(process.env.QUANTITY);
+    }
 
     const createAppointmentService = container.resolve(
       CreateAppointmentService,
     );
     const appointmentUser = await createAppointmentService.execute({
       user_id: id,
+      birth,
       date,
+      quantity,
+      duration,
+      daysRange,
+      dayQuantityLimit,
     });
 
     return response.status(201).json({ appointmentUser });
