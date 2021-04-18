27fd7c5ae945542291943e464cf6577d806e685f
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-17 22:38:19 -0300

    feat: add route for getting future appointments for users

--- src/modules/appointment/infra/http/routes/appointments.routes.ts
+++ src/modules/appointment/infra/http/routes/appointments.routes.ts
@@ -1,15 +1,23 @@
 import { Router } from 'express';
 import ensureUserAuthenticated from '../../../../user/infra/http/middlewares/ensureUserAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
+import UsersAvailabilityController from '../controllers/UsersAvailabilityController';
 
 const appointmentsRouter = Router();
 
 const appointmentsController = new AppointmentsController();
+const usersAvailabilityController = new UsersAvailabilityController();
 
 appointmentsRouter.post(
   '/',
   ensureUserAuthenticated,
   appointmentsController.create,
+);
+
+appointmentsRouter.get(
+  '/userAvailability',
+  ensureUserAuthenticated,
+  usersAvailabilityController.index,
 );
 
 export default appointmentsRouter;
