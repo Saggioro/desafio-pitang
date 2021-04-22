d2333f5793b7d13c80be4f6ce820f9a3561c4f21
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-22 00:09:37 -0300

    feat: add new controller to get appointments availabity

---
+++ src/modules/appointment/infra/http/controllers/AppointmentsAvailabilityController.ts
@@ -0,0 +1,62 @@
+import { container } from 'tsyringe';
+import { Request, Response } from 'express';
+import GetAppointmentAvailabilityService from '../../../services/GetAppointmentAvailabilityService';
+
+class AppointmentsAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    let daysRange = 15;
+    let quantity = 2;
+    let duration = 30;
+    let dayQuantityLimit = 20;
+
+    if (
+      process.env.DAY_QUANTITY_LIMIT &&
+      !Number.isNaN(process.env.DAY_QUANTITY_LIMIT) &&
+      Number(process.env.DAY_QUANTITY_LIMIT) >= 1
+    ) {
+      dayQuantityLimit = Number(process.env.DAY_QUANTITY_LIMIT);
+    }
+    if (
+      (process.env.RANGE &&
+        !Number.isNaN(process.env.RANGE) &&
+        Number(process.env.RANGE) === 10) ||
+      Number(process.env.RANGE) === 15 ||
+      Number(process.env.RANGE) === 20
+    ) {
+      daysRange = Number(process.env.RANGE);
+    }
+
+    if (
+      (process.env.DURATION &&
+        !Number.isNaN(process.env.DURATION) &&
+        Number(process.env.DURATION) === 10) ||
+      Number(process.env.DURATION) === 15 ||
+      Number(process.env.DURATION) === 20 ||
+      Number(process.env.DURATION) === 30
+    ) {
+      duration = Number(process.env.DURATION);
+    }
+
+    if (
+      process.env.QUANTITY &&
+      !Number.isNaN(process.env.QUANTITY) &&
+      Number(process.env.QUANTITY) >= 1
+    ) {
+      quantity = Number(process.env.QUANTITY);
+    }
+    const getAppointmentAvailabilityService = container.resolve(
+      GetAppointmentAvailabilityService,
+    );
+
+    const appointments = await getAppointmentAvailabilityService.execute({
+      quantity,
+      duration,
+      daysRange,
+      dayQuantityLimit,
+    });
+
+    return response.json(appointments);
+  }
+}
+
+export default AppointmentsAvailabilityController;
