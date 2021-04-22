01c607b7a3f43be7caee387fd362f5be2ad64d60
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-22 00:08:04 -0300

    fix: availability is a diferent controller now

--- src/modules/appointment/infra/http/routes/appointments.routes.ts
+++ src/modules/appointment/infra/http/routes/appointments.routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import ensureUserAuthenticated from '../../../../user/infra/http/middlewares/ensureUserAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
 import UsersAvailabilityController from '../controllers/UsersAvailabilityController';
+import AppointmentsAvailabilityController from '../controllers/AppointmentsAvailabilityController';
 
 const appointmentsRouter = Router();
 
 const appointmentsController = new AppointmentsController();
 const usersAvailabilityController = new UsersAvailabilityController();
+const appointmentsAvailabilityController = new AppointmentsAvailabilityController();
 
 appointmentsRouter.post(
   '/',
@@ -15,9 +17,9 @@ appointmentsRouter.post(
 );
 
 appointmentsRouter.get(
-  '/userAvailability',
-  ensureUserAuthenticated,
-  usersAvailabilityController.index,
+  '/availability',
+
+  appointmentsAvailabilityController.index,
 );
 
 export default appointmentsRouter;
