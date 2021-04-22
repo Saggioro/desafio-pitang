e0e79410773304a85b893a1f2f9cf19e046be7bd
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-22 00:08:43 -0300

    feat: add cors to server file

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import 'express-async-errors';
 import 'dotenv/config';
+import cors from 'cors';
 
 import express, { Request, Response, NextFunction } from 'express';
 import AppError from '../../errors/AppError';
@@ -11,7 +12,7 @@ import '../typeorm';
 import '../../container';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
