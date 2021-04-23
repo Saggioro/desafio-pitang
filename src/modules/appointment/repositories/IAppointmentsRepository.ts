9f7c5b0d441b09ade58e69dcd2f9083c27c6bf4b
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-23 11:44:55 -0300

    fix: fix method getByDay to receive the start and of a day instead of just a date

--- src/modules/appointment/repositories/IAppointmentsRepository.ts
+++ src/modules/appointment/repositories/IAppointmentsRepository.ts
@@ -5,7 +5,7 @@ interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   save(appointment: Appointment): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
-  findByDay(date: Date): Promise<Appointment[]>;
+  findByDay(startOfDay: Date, endOfDay: Date): Promise<Appointment[]>;
   findAllMoreThan(date: Date): Promise<Appointment[]>;
 }
 
