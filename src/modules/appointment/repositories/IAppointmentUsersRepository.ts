4610e23aaffe29c382116f54d7969b8df6c32ec6
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-23 23:02:31 -0300

    feat: add method getById to appointmentUsersRepository interface

--- src/modules/appointment/repositories/IAppointmentUsersRepository.ts
+++ src/modules/appointment/repositories/IAppointmentUsersRepository.ts
@@ -5,6 +5,7 @@ interface IAppointmentUsersRepository {
   findByUser(user_id: string): Promise<AppointmentUser[]>;
   create(data: ICreateAppointmentUserDTO): Promise<AppointmentUser>;
   save(appointmentUser: AppointmentUser): Promise<AppointmentUser>;
+  findById(id: string): Promise<AppointmentUser | undefined>;
 }
 
 export default IAppointmentUsersRepository;
