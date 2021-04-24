b430d45d4bd9bd283a3b57df3d3bb43f946816ff
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-23 23:10:19 -0300

    feat: add routes to get appointments,delete and update

--- src/modules/appointment/infra/http/routes/appointments.routes.ts
+++ src/modules/appointment/infra/http/routes/appointments.routes.ts
@@ -1,14 +1,19 @@
 import { Router } from 'express';
+import ensureNurseAuthenticated from '../../../../nurse/infra/http/middlewares/ensureNurseAuthenticated';
 import ensureUserAuthenticated from '../../../../user/infra/http/middlewares/ensureUserAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
-import UsersAvailabilityController from '../controllers/UsersAvailabilityController';
+import FutureAppointmentsController from '../controllers/FutureAppointmentsController';
 import AppointmentsAvailabilityController from '../controllers/AppointmentsAvailabilityController';
+import AppointmentUserController from '../controllers/AppointmentUserController';
+import TodayAppointmentsController from '../controllers/TodayAppointmentsController';
 
 const appointmentsRouter = Router();
 
 const appointmentsController = new AppointmentsController();
-const usersAvailabilityController = new UsersAvailabilityController();
+const futureAppointmentsController = new FutureAppointmentsController();
 const appointmentsAvailabilityController = new AppointmentsAvailabilityController();
+const appointmentUserController = new AppointmentUserController();
+const todayAppointmentsController = new TodayAppointmentsController();
 
 appointmentsRouter.post(
   '/',
@@ -17,9 +22,33 @@ appointmentsRouter.post(
 );
 
 appointmentsRouter.get(
+  '/',
+  ensureUserAuthenticated,
+  appointmentsController.find,
+);
+
+appointmentsRouter.get(
   '/availability',
   ensureUserAuthenticated,
   appointmentsAvailabilityController.index,
+);
+
+appointmentsRouter.delete(
+  '/user/:id',
+  ensureUserAuthenticated,
+  appointmentUserController.delete,
+);
+
+appointmentsRouter.put(
+  '/provide',
+  ensureNurseAuthenticated,
+  appointmentUserController.update,
+);
+
+appointmentsRouter.get(
+  '/today',
+  ensureNurseAuthenticated,
+  todayAppointmentsController.index,
 );
 
 export default appointmentsRouter;
